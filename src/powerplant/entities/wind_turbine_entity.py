f12c6ca8619a851b2a34c56b1ddf4d1641b471b7
Clément MARCEL <lecramproject@gmail.com>
2024-10-29 14:32:12 +0100

    feat: create entities

---
+++ src/powerplant/entities/wind_turbine_entity.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from src.powerplant.entities.power_plant_entity import PowerPlant
+
+
+class WindTurbine(PowerPlant):
+    def calculate_cost_per_mwh(
+        self, fuel_price: Optional[float] = None, co2_price: Optional[float] = 0
+    ) -> float:
+        return 0
+
+    def calculate_production(
+        self, load: float, wind_percentage: Optional[float] = 0
+    ) -> float:
+        if not wind_percentage:
+            raise ValueError("No wind percentage provided")
+        return min(self.pmax, self.pmax * (wind_percentage / 100))
