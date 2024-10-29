d6acaa4a8c307ee3fc45d925773deac50826ffaa
Clément MARCEL <lecramproject@gmail.com>
2024-10-29 14:28:25 +0100

    feat: add get production plan usecase and unit test

--- src/powerplant/usecases/get_production_plan_usecase.py
+++ src/powerplant/usecases/get_production_plan_usecase.py
@@ -1,16 +1,19 @@
-from typing import List, Dict
+from typing import List, Dict, TypedDict
 
 from src.powerplant.entities.gas_fired_plant_entity import GasFiredPlant
 from src.powerplant.entities.power_plant_entity import PowerPlant
 from src.powerplant.entities.turbo_jet_entity import TurboJet
-from src.powerplant.entities.wind_turbine_entity import WindTurbine
+
+Plan = TypedDict("Plan", {"name": str, "p": float})
 
 
 class ProductionPlan:
     def __init__(self, plants: List[PowerPlant]):
         self.plants: List[PowerPlant] = plants
 
-    def execute(self, load: float, fuel_prices: Dict[str, float], wind_percentage: float) -> List[Dict[str, float]]:
+    def execute(
+        self, load: float, fuel_prices: Dict[str, float], wind_percentage: float
+    ) -> List[Plan]:
         self._calculate_production_costs(fuel_prices)
 
         self.plants.sort(key=lambda plant: plant.cost)
@@ -25,16 +28,18 @@ class ProductionPlan:
         """Calculates the cost per MWh for each power station according to its type and fuel prices."""
         for plant in self.plants:
             if isinstance(plant, GasFiredPlant):
-                plant.cost = plant.calculate_cost_per_mwh(fuel_prices["gas"], fuel_prices["co2"])
+                plant.cost = plant.calculate_cost_per_mwh(
+                    fuel_prices["gas"], fuel_prices["co2"]
+                )
                 continue
             elif isinstance(plant, TurboJet):
                 plant.cost = plant.calculate_cost_per_mwh(fuel_prices["kerosine"])
                 continue
             plant.cost = plant.calculate_cost_per_mwh()
 
-    def _allocate_production(self, load: float, wind_percentage: float) -> List[Dict[str, float]]:
+    def _allocate_production(self, load: float, wind_percentage: float) -> List[Plan]:
         """Allocates production to meet the requested workload, respecting the order of merit"""
-        production_plan: List[Dict[str, float]] = []
+        production_plan: List[Plan] = []
         total_production: float = 0.0
 
         for plant in self.plants:
@@ -42,7 +47,9 @@ class ProductionPlan:
                 break
 
             # Calculate production for each plant
-            production = self._calculate_plant_production(plant, load - total_production, wind_percentage)
+            production = self._calculate_plant_production(
+                plant, load - total_production, wind_percentage
+            )
 
             if production >= plant.pmin:
                 production_plan.append({"name": plant.name, "p": production})
@@ -51,13 +58,14 @@ class ProductionPlan:
         return production_plan
 
     @staticmethod
-    def _calculate_plant_production( plant: PowerPlant, remaining_load: float, wind_percentage: float) -> float:
-        """Calculates the production for a specific plant, taking into account its limits. """
+    def _calculate_plant_production(
+        plant: PowerPlant, remaining_load: float, wind_percentage: float
+    ) -> float:
+        """Calculates the production for a specific plant, taking into account its limits."""
         production = plant.calculate_production(remaining_load, wind_percentage)
         return round(min(production, plant.pmax), 1)
 
-
-    def _include_non_active_plants(self, production_plan: List[Dict[str, float]]) -> None:
+    def _include_non_active_plants(self, production_plan: List[Plan]) -> None:
         """Adds unused power plants to the production plan with a production of 0.0"""
         for plant in self.plants:
             if all(entry["name"] != plant.name for entry in production_plan):
--- src/powerplant/usecases/test_get_production_plan_usecase.py
+++ src/powerplant/usecases/test_get_production_plan_usecase.py
@@ -1,25 +1,28 @@
 from typing import Dict
 
+import pytest
+
 from src.powerplant.entities.factories.powerplant_factories import PowerPlantFactory
-from src.powerplant.usecases.get_production_plans_usecase import ProductionPlan
+from src.powerplant.usecases.get_production_plan_usecase import ProductionPlan
 
 
+@pytest.mark.unit
 def test_production_plan_usecase() -> None:
     # GIVEN
     load = 480
-    fuel_prices: Dict[str, float] = {
-        "gas": 20,
-        "kerosine": 50,
-        "co2": 10
-    }
+    fuel_prices: Dict[str, float] = {"gas": 20, "kerosine": 50, "co2": 10}
     wind_percentage = 60
 
     # Create a list of power plants
     plants = [
-        PowerPlantFactory.create_gas_fired(name="gasplant1", efficiency=0.5, pmin=100, pmax=300),
-        PowerPlantFactory.create_turbojet(name="turbojet1", efficiency=0.3, pmin=0, pmax=200),
+        PowerPlantFactory.create_gas_fired(
+            name="gasplant1", efficiency=0.5, pmin=100, pmax=300
+        ),
+        PowerPlantFactory.create_turbojet(
+            name="turbojet1", efficiency=0.3, pmin=0, pmax=200
+        ),
         PowerPlantFactory.create_wind_turbine(name="windturbine1", pmax=150),
-        PowerPlantFactory.create_wind_turbine(name="windturbine2", pmax=36)
+        PowerPlantFactory.create_wind_turbine(name="windturbine2", pmax=36),
     ]
 
     # WHEN
@@ -31,6 +34,6 @@ def test_production_plan_usecase() -> None:
         {"name": "windturbine1", "p": 90.0},
         {"name": "windturbine2", "p": 21.6},
         {"name": "gasplant1", "p": 300.0},
-        {"name": "turbojet1", "p": 68.4}
+        {"name": "turbojet1", "p": 68.4},
     ]
     assert result == expected_result, f"Expected {expected_result} but got {result}"
